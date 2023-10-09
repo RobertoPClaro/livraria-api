45e588ad102f9f6d528e7abb98ffbc88ef00c3bd
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2023-10-09 18:07:50 -0300

    Adicionando Autores e refatorando Livros

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import "dotenv/config.js"
+import "dotenv/config";
 import app from "./src/app.js";
 
 const PORT = 3000;
 
-app.listen(PORT, () => { //Esta ouvindo o servidor na porta 3000
-    console.log("servidor escutando!");
+app.listen(PORT, () => {
+  console.log("servidor escutando!");
 });
