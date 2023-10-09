53cb2751e50ec8637eb12f49ee34afd5f2f4aeb8
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2023-10-09 09:48:52 -0300

    Refatorando e adicinando models, e conexão com o banco de dados moongo

--- src/app.js
+++ src/app.js
@@ -1,60 +1,49 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexao com o banco feita com sucesso");
+})
 
 const app = express();
 app.use(express.json());
 
-const livros = [
-    {
-        id: 1,
-        titulo: "O Senhor dos áneis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    },
-    {
-        id: 3,
-        titulo: "Harry Potter e a Pedra Filosofal"
-    },
-    {
-        id: 4,
-        titulo: "Harry Potter e a Câmara Secreta"
-    }
-]
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    });
-}
-
-app.get("/", (requisicao, resposta) => {
-    resposta.status(200).send("Curso de Nodew.Js");
+app.get("/", (req, res) => {
+  res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (requisicao, resposta) => {
-    resposta.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+  const listaLivros = await livro.find({});
+  res.status(200).json(listaLivros);
 });
 
-app.get("/livros/:id", (requisicao, resposta) => {
-    const index = buscaLivro(requisicao.params.id);
-    resposta.status(200).json(livros[index]);
-});
-
-app.post("/livros", (requisicao, resposta) => {
-    livros.push(requisicao.body);
-    resposta.status(201).send("Livro cadastrado com sucesso");
-});
-
-app.put("/livros/:id", (requisicao, resposta) => {
-    const index = buscaLivro(requisicao.params.id);
-    livros[index].titulo = requisicao.body.titulo;
-    resposta.status(200).json(livros[index]);
-});
-
-app.delete("/livros/:id", (requisicao, resposta) => {
-    const index = buscaLivro(requisicao.params.id);
-    livros.splice(index, 1);
-    resposta.status(200).send("Livro removido com sucesso");
+app.get("/livros/:id", (req, res) => {
+  const livroDb = livro.findById(req.params.id);
+  res.status(200).json(livroDb);
 })
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("livro cadastrado com sucesso");
+});
+
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json(livros);
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("livro removido com sucesso");
+});
+
 export default app;
